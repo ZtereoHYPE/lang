c4e0ec49c659e990c5d55ce5dffd768630d7a616
Saksham564 <khatkarsaksham0@gmail.com>
2026-01-11 21:09:13 +0100

    Update live_var_panel.rs

--- src/tui/live_var_panel.rs
+++ src/tui/live_var_panel.rs
@@ -1,28 +1,27 @@
-use std::collections::HashMap;
+use crate::states::ast::Identifier;
+use crate::tui::{BACKGROUND, BLOCK, BORDER_STYLE};
 use itertools::Itertools;
 use ratatui::prelude::*;
 use ratatui::style::palette::tailwind;
-use ratatui::widgets::{Block, BorderType, Borders, Cell, HighlightSpacing, List, ListState, Padding, Row, Table};
-use crate::tui::{BACKGROUND, BLOCK, BORDER_STYLE};
+use ratatui::widgets::{Cell, Row, Table};
+use std::collections::HashSet;
 
 pub struct LiveVarPanel {
-    live_variables: HashMap<String, i32>, // todo: this is just a placeholder
+    live_variables: HashSet<Identifier>,
 }
 
 impl Widget for LiveVarPanel {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from(vec![
-            "Stored Vars - ".white(),
-            self.live_variables.len().bold().blue(),
-            " Entries".bold().white()
-        ]).centered();
+            "Live Variables - ".white(),
+            self.live_variables.len().to_string().bold().blue().into(),
+            " Entries".bold().white(),
+        ])
+        .centered();
 
-        let block = BLOCK
-            .title(title)
-            .bg(BACKGROUND)
-            .border_style(BORDER_STYLE);
+        let block = BLOCK.title(title).bg(BACKGROUND).border_style(BORDER_STYLE);
 
-        let header = ["#", "Name", "Value"]
+        let header = ["#", "Variable"]
             .into_iter()
             .map(Cell::from)
             .collect::<Row>()
@@ -30,23 +29,24 @@ impl Widget for LiveVarPanel {
             .white()
             .height(1);
 
-        let rows = self.live_variables
+        let rows = self
+            .live_variables
             .iter()
-            .sorted()
+            .sorted_by_key(|id| &id.id)
             .enumerate()
-            .map(|(idx, (s, i))| {
+            .map(|(idx, id)| {
                 let color = match idx % 2 {
                     0 => tailwind::NEUTRAL.c800,
-                    _ => tailwind::NEUTRAL.c900
+                    _ => tailwind::NEUTRAL.c900,
                 };
                 Row::new(vec![
                     Cell::from(Text::from(idx.to_string())),
-                    Cell::from(Text::from(s.clone())),
-                    Cell::from(Text::from(i.to_string())),
-                ]).bg(color)
+                    Cell::from(Text::from(id.id.clone())),
+                ])
+                .bg(color)
             });
 
-        let table = Table::new(rows, [Constraint::Length(3), Constraint::Fill(1), Constraint::Fill(1)])
+        let table = Table::new(rows, [Constraint::Length(3), Constraint::Fill(1)])
             .header(header)
             .block(block);
 
@@ -55,7 +55,7 @@ impl Widget for LiveVarPanel {
 }
 
 impl LiveVarPanel {
-    pub(crate) fn new(live_variables: HashMap<String, i32>) -> Self {
+    pub fn new(live_variables: HashSet<Identifier>) -> Self {
         Self { live_variables }
     }
 }
